ceccb12ac02e6862231a0f649db22b797135cb05
unknown <grantludwig97@gmail.com>
2019-04-15 21:51:06 -0700

    generated final dates, changed proper date

--- hw2.py
+++ hw2.py
@@ -14,25 +14,39 @@ def isLeapYear(year):
         return true
     return False
 
-#TODO
+def maxMonthDay(month, year):
+    if month == 2:
+        if isLeapYear(year):
+            return 29
+        else:
+            return 28
+    elif month == 4 or month == 6 or month == 9 or month == 11:
+        return 30
+    return 31
+
 def properDate(date):
     month, day, year = date
     if year < 1800:
         raise ImproperDate
     elif month < 1 or month > 12:
         raise ImproperDate
-    elif day < 1 or day > 31:
+    elif day < 1 or day > maxMonthDay(month, year):
         raise ImproperDate
-    elif day > 30:
-        if month == 4 or month == 6 or month == 9 or month == 11:
-            raise ImproperDate
-    elif month == 2:
-        if isLeapYear(year):
-            if day > 29:
-                raise ImproperDate
-        else:
-            if day > 28:
-                raise ImproperDate
+    ##elif month < 1 or month > 12:
+    ##    raise ImproperDate
+    ##elif day < 1 or day > 31:
+    ##    raise ImproperDate
+    ##elif day > 30:
+    ##    if month == 4 or month == 6 or month == 9 or month == 11:
+    ##        raise ImproperDate
+    ##elif month == 2:
+    ##    if isLeapYear(year):
+    ##        if day > 29:
+    ##            raise ImproperDate
+    ##    else:
+    ##        if day > 28:
+    ##            raise ImproperDate
+
 
 def createDateList(fileName):
     list = []
@@ -53,6 +67,15 @@ def createDateList(fileName):
         raise BadFileFormat
     return list
 
+
+
+def genFinalDates(year):
+    list = []
+    for month in range(1,13):
+        date = (month, maxMonthDay(month, year), year)
+        list.append(date)
+    return list
+
 fileName = sys.argv[1]
 try:
     list = createDateList(fileName)
@@ -61,3 +84,5 @@ except BadFileFormat:
     print("BadFileFormat")
 except ImproperDate:
     print("ImproperDate")
+dateList = genFinalDates(2000)
+print(dateList)
